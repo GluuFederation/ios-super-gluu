35ed3b1642bb52feefe8828d9332aff58831aac3
Eric Webb <ericwebb85@yahoo.com>
2020-01-02 17:17:31 -0600

    cleanup

--- AppInfo.swift
+++ AppInfo.swift
@@ -5,7 +5,7 @@ import Foundation
 class AppInfo {
     let version: String
     let build: String
-    let gitCommitSHA: String = "57086f1"
+    let gitCommitSHA: String = "8545e29"
     
     init?() {
         guard let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String,
